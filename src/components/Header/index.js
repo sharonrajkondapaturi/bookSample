aca295ebdb1769227168b896f4a03acf2811a184
sharonrajkondapaturi <sharoncheran@gmail.com>
2024-10-04 00:48:36 +0530

    final-commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,18 @@
+import {useNavigate} from 'react'
 import './index.css'
 
 //This component is used as a Header to redirect to respective pages
 const Header = ()=>{
+    const navigate = useNavigate()
+    const onLib = ()=>{
+        navigate('/myLibrary')
+    }
 return(
     <>
     <header className='nav-back'>
         <nav>
             <a className="nav-ele" href="/">Home</a>
-            <a className="nav-ele" href="/myLibrary">My Library</a>
+            <a className="nav-ele" href="/myLibrary"onClick={onLib}>My Library</a>
         </nav>
     </header>
     </>
